b298471a00303714104224dae010b3aaeed595d4
atom-data <alatatomondi@gmail.com>
2023-01-17 09:55:31 +0000

    updated number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,48 +1,65 @@
 #!/usr/bin/python3
-"""5-number_template.py"""
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
+    /number_template/<n>: Displays an HTML page only if <n> is an integer.
+"""
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """hello_hbnb"""
-    return 'Hello HBNB!'
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
 
-@app.route('/nbnb/', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """hbnb"""
-    return 'HBNB'
+    """Displays 'HBNB'"""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def cWithVariable(text):
-    """cWithVariable"""
-    text = text.replace('_', ' ')
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
     return "C {}".format(text)
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>/', strict_slashes=False)
-def pythonWithVariable(text="is cool"):
-    """pythonWithVariable"""
-    text = text.replace('_', ' ')
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
     return "Python {}".format(text)
 
 
-@app.route('/number/<int:n>/', strict_slashes=False)
-def withNumber(n):
-    """withNumber"""
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Displays 'n is a number' only if <n> is an integer."""
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>/', strict_slashes=False)
-def withNumberSendWebpage(n=None):
-    """withNumberSendWebpage"""
-    return render_template('5-number.html', n=n)
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Displays an HTML page only if <n> is an integer."""
+    return render_template("5-number.html", n=n)
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
